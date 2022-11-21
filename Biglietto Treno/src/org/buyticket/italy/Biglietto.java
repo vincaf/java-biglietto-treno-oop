2e631dd5295b01f189bb2a0e11ea1809e74e5e8d
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-21 14:55:26 +0100

    feat(Biglietto): add variables w/ methods, getters and setters
    initialized userKm and userAge at 0, created constants and add methods to control if km and age are valids

--- Biglietto Treno/src/org/buyticket/italy/Biglietto.java
+++ Biglietto Treno/src/org/buyticket/italy/Biglietto.java
@@ -1,5 +1,62 @@
 package org.buyticket.italy;
 
-public class Biglietto {
+import java.math.BigDecimal;
 
+public class Biglietto {
+	
+	private int userKm = 0;
+	private int userAge = 0;
+	
+	private static final BigDecimal PRICE_FOR_KM = new BigDecimal(0.21);
+	private static final BigDecimal OVER_65_DISCOUNT = new BigDecimal(0.6);
+	private static final BigDecimal YOUNG_DISCOUNT = new BigDecimal(0.8);
+	
+	Biglietto(int userKm, int userAge, boolean flexible) throws Exception{
+		isValidKm(userKm);
+		isValidEta(userAge);
+	}
+
+	public int getUserKm() {
+		return userKm;
+	}
+
+	public void setUserKm(int userKm) {
+		this.userKm = userKm;
+	}
+
+	public int getUserAge() {
+		return userAge;
+	}
+
+	public void setUserAge(int userAge) {
+		this.userAge = userAge;
+	}
+	
+	public static BigDecimal getPriceForKm() {
+		return PRICE_FOR_KM;
+	}
+	
+	public static BigDecimal getOver65Discount() {
+		return OVER_65_DISCOUNT;
+	}
+	
+	public static BigDecimal getYoungDiscount() {
+		return YOUNG_DISCOUNT;
+	}
+	
+	public void isValidKm(int km) throws Exception {
+		if(km < 0) {
+			throw new Exception("Kilometers must be positive number");
+		} else {
+			this.userKm = km;
+		}
+	}
+	
+	public void isValidEta(int eta) throws Exception {
+		if (eta <= 0) {
+			throw new Exception("Age must be positive and not equal to 0");
+		} else {
+			this.userAge = eta;
+		}
+	}
 }
